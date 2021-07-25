caa7fc8546a8cf3bf5a87a000f32e2098d2bdae7
Shreya M Biradar <biradarshreya24@gamil.com>
2021-07-25 14:22:39 +0530

    added html and css files

--- hacktable2021/script.js
+++ hacktable2021/script.js
@@ -1,15 +1,7 @@
-var item = document.querySelectorAll(".item");
-var item1 = document.querySelector(".item");
-var toggle = document.querySelector(".toggle");
+const toggle = document.querySelector(".toggle");
+const navigation = document.querySelector(".navigation");
 
 toggle.addEventListener("click", () => {
-  if (item1.classList.contains("active")) {
-    for (var i = 0; i < item.length; i++) {
-      item[i].classList.remove("active");
-    }
-  } else {
-    for (var i = 0; i < item.length; i++) {
-      item[i].classList.add("active");
-    }
-  }
+  toggle.classList.toggle("active");
+  navigation.classList.toggle("active");
 });
